cc45c7b17ca290e07819cd79afc3f031c36af309
Marco André Mendes <marcoandre@gmail.com>
2024-03-15 08:58:28 -0300

    Formatação com isort e black

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 """
 Django admin customization.
 """
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -46,6 +47,7 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
+
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Categoria)
