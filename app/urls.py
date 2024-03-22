31f72a101536819a2642f30db1fc57ca657ff2f0
Marco André Mendes <marcoandre@gmail.com>
2024-03-22 08:42:20 -0300

    Criação da API do Autor

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet
 
 router = DefaultRouter()
 
+router.register(r"autores", AutorViewSet, basename="autores")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"editoras", EditoraViewSet, basename="editoras")
-router.register(r"usuarios", UserViewSet, basename="users")
+router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
