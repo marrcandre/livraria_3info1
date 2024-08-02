c5f5847eca4ef835d086f2a39aaeee111b35997a
Marco André Mendes <marcoandre@gmail.com>
2024-08-02 08:45:43 -0300

    feat: Adiciona relacionamento de Livro com Autor e Coautor

--- core/models/livro.py
+++ core/models/livro.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from .categoria import Categoria
 from .editora import Editora
+from .autor import Autor
 
 
 class Livro(models.Model):
@@ -11,6 +12,9 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livrosAutor", blank=True, null=True) # livro com 1 autor
+    coautor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livrosCoautor", blank=True, null=True) # livro com 1 coautor
+
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
