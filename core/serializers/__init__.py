4c9392d6ecf1444438e135b1062cbbedbb8da880
Marco André Mendes <marcoandre@gmail.com>
2024-09-27 08:43:54 -0300

    Criação de um endpoint para criar novas compras.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,11 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer
+from .compra import (
+    CompraSerializer,
+    CriarEditarCompraSerializer,
+    CriarEditarItensCompraSerializer,
+    ItensCompraSerializer,
+)
 from .editora import EditoraSerializer
 from .livro import LivroDetailSerializer, LivroListSerializer, LivroSerializer
 from .user import UserSerializer
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -3,7 +3,6 @@ from rest_framework.serializers import CharField, SerializerMethodField, ModelSe
 from core.models import Compra, ItensCompra
 
 
-
 class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
 
@@ -16,6 +15,12 @@ class ItensCompraSerializer(ModelSerializer):
         depth = 1
 
 
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
@@ -24,3 +29,19 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ("id", "usuario", "status", "total", "itens")
+
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
